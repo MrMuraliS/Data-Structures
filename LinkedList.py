431519d45ec1481f1a36259bfcc8b6490b42cdae
muralis <muralis1223@gmail.com>
2022-05-09 18:50:51 +0530

    adding other required linked in functions

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,6 @@
+from tkinter.messagebox import NO
+
+
 class Node:
 
     def __init__(self, value) -> None:
@@ -29,6 +32,7 @@ class LinkedList:
         node.next = self.head
         self.head = node
         self.lenght += 1
+        return self.head.__dict__
 
     def pop_first(self):
         if self.lenght == 0:
@@ -42,8 +46,20 @@ class LinkedList:
             self.tail = None
         return temp.__dict__
 
-    def insert(self, value):
-        pass
+    def insert(self,index, value):
+        if index < 0 or index > self.lenght:
+            return False
+        if index == 0:
+            return self.preappend(value)
+        if index == self.lenght:
+            return self.append(value)
+        node = Node(value)
+        temp = self.get(index-1)
+        node.next = temp.next
+        temp.next = node
+        self.lenght += 1
+        return True
+
 
     def pop(self):
         if self.lenght == 0:
@@ -75,7 +91,7 @@ class LinkedList:
         temp = self.head
         for _ in range(index):
             temp = temp.next
-        return temp.value
+        return temp
 
     def set_value(self, index, value):
         if index < 0 or index > self.lenght:
@@ -87,11 +103,37 @@ class LinkedList:
         temp.value = value
         return temp.__dict__
 
+    def remove(self, index):
+        if index < 0 or index >= self.lenght:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.lenght - 1:
+            return self.pop()
+        pre = self.get(index-1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.lenght -= 1
+        return temp
+
+    def reverse(self):
+        temp = self.head.next
+        print(temp.__dict__)
+
 
 my_linked_list = LinkedList(4)
 my_linked_list.append(7)
 my_linked_list.preappend(22)
-my_linked_list.append(8)
-my_linked_list.preappend(15)
-my_linked_list.preappend(999)
+
+
+
+my_linked_list.insert(3,1)
+
+my_linked_list.remove(2)
+
+my_linked_list.print_list()
+
 print('\nThe length is:', my_linked_list.lenght)
+
+print(my_linked_list.reverse())
