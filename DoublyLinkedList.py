ccd4d77112b6900a15122067149edea800e3ebe1
muralis <muralis1223@gmail.com>
2022-05-10 11:39:56 +0530

    Adding Stack

---
+++ DoublyLinkedList.py
@@ -0,0 +1,81 @@
+from tkinter.messagebox import NO
+
+
+class Node:
+
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self, value):
+        node = Node(value)
+        self.head = node
+        self.tail = node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        temp.prev = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def preappend(self, value):
+        node = Node(value)
+        if self.length == 0:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node 
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+
+doubly_linked_list = DoublyLinkedList(5)
+doubly_linked_list.append(10)
+doubly_linked_list.preappend(77)
+doubly_linked_list.print_list()
