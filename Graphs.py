18f925c9f8961c9e8cb4f580f6a209c83d8cbbaa
muralis <muralis1223@gmail.com>
2022-05-13 12:27:30 +0530

    Adding Graphs

--- Graphs.py
+++ Graphs.py
@@ -19,11 +19,31 @@ class Graph:
             return True
         return False
 
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertix(self, vertix):
+        if vertix in self.adj_list.keys():
+            for other_vertix in self.adj_list[vertix]:
+                self.adj_list[other_vertix].remove(vertix)
+            del self.adj_list[vertix]
+            return True
+        return False
+
 
 my_graph = Graph()
 my_graph.add_vertex(1)
 my_graph.add_vertex(2)
 
 my_graph.add_edge(1,2)
+my_graph.remove_edge(2,3)
+my_graph.remove_edge(1,2)
 
 my_graph.print_graph()
